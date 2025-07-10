32ccd0bc256f51ee3debb9b62ccb8560057bd135
Vansh Tarar <vanshtarar4@gmail.com>
2025-07-10 15:03:16 +0530

    testing

--- src/main/java/com/celebal/pharmaapp/controller/WebController.java
+++ src/main/java/com/celebal/pharmaapp/controller/WebController.java
@@ -14,7 +14,7 @@ public class WebController {
     @GetMapping("/")
     public String home(Model model) {
         List<Product> productList = Arrays.asList(
-            new Product(1L, "Paracetamol(Vansh)", "Pain reliever and a fever reducer.", "Take 1-2 tablets every 4-6 hours as needed.", "Do not exceed 8 tablets in 24 hours. Not for children under 6."),
+            new Product(1L, "Paracetamol", "Pain reliever and a fever reducer.", "Take 1-2 tablets every 4-6 hours as needed.", "Do not exceed 8 tablets in 24 hours. Not for children under 6."),
             new Product(2L, "Cetirizine", "Antihistamine for allergy relief.", "Take 1 tablet once daily.", "May cause drowsiness. Not to be used with alcohol."),
             new Product(3L, "Amoxicillin", "Antibiotic for bacterial infections.", "Take 1 capsule every 8 hours for 7 days.", "Complete the course. Not for viral infections."),
             new Product(4L, "Ibuprofen", "Nonsteroidal anti-inflammatory drug (NSAID) for pain and inflammation.", "Take 1-2 tablets every 6-8 hours as needed.", "Take with food. Do not exceed 6 tablets in 24 hours."),
