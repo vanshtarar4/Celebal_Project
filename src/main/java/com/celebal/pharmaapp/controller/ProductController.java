ea7ed964868265d6863eebcf17bdc4a5f2676d2d
Vansh Tarar <vanshtarar4@gmail.com>
2025-07-10 10:15:05 +0530

    update test

--- src/main/java/com/celebal/pharmaapp/controller/ProductController.java
+++ src/main/java/com/celebal/pharmaapp/controller/ProductController.java
@@ -19,6 +19,7 @@ public class ProductController {
     new Product(6L, "Azithromycin", "Antibiotic for respiratory infections.", "Take 1 tablet daily for 3 days.", "Complete full course. May cause stomach upset."),
     new Product(7L, "Loratadine", "Antihistamine for hay fever and allergies.", "Take 1 tablet once daily.", "Consult doctor if pregnant."),
     new Product(8L, "Omeprazole", "Proton pump inhibitor for acid reflux.", "Take 1 capsule before breakfast.", "Do not crush or chew capsules.")
+    new Product(9L, "test", "this is a test case ", "it is generated for the testing purpose only", "Do not consider this as the part of the app")
     );
 
     @GetMapping
